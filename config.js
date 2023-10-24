5257927e57ce4cfeca120163c810dbd96fa2018c
MatiBatt <battitimatias@gmail.com>
2023-10-24 16:02:56 +0000

    Insert API file

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const NEWS_API_KEY = 'pub_316740aaa6eee09a8655bd507c8abf66e5037';
-export const WEATHER_API_KEY = 'c4a6cfa4d637dc8684b0295b4d20e5f9';
+export const NEWS_API_KEY = ''; // INSERT YOUR API KEY
+export const WEATHER_API_KEY = ''; // INSERT YOUR API KEY
