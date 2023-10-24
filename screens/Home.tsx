62b4adb1b5bf17b810e952969e96fcfba6ccfe13
MatiBatt <battitimatias@gmail.com>
2023-10-24 11:43:00 -0300

    Weather Widget added

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -3,18 +3,43 @@ import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
 import { Appbar, Chip, Button, ProgressBar, MD3Colors } from 'react-native-paper';
 import { ComponentNavigationProps, NewsData } from '../utils/types';
 import CardItem from '../components/CardItem';
+import Icon from 'react-native-vector-icons/FontAwesome'; // Change to your preferred icon library
 
 const categories = ["Technology", "Sports", "Politics", "Health", "Business"];
 const API_KEY = 'pub_316740aaa6eee09a8655bd507c8abf66e5037';
+const placeholderImage = 'https://pioneer-technical.com/wp-content/uploads/2016/12/news-placeholder.png';
 
-const placeholderImage = 'https://pioneer-technical.com/wp-content/uploads/2016/12/news-placeholder.png'; // Replace with your actual placeholder image URL
+const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Buenos%20Aires,AR&units=metric&appid=c4a6cfa4d637dc8684b0295b4d20e5f9`;
 
 const Home = (props: ComponentNavigationProps) => {
   const [newsData, setNewsData] = useState<NewsData[]>([]);
   const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [nextPage, setNextPage] = useState("");
+  const [weather, setWeather] = useState<any>(null);
 
+  useEffect(() => {
+    fetchWeatherData()
+      .then((data) => {
+        if (data.cod === 200 && data.main && data.main.temp) {
+          setWeather(data);
+        } else {
+          console.error('Invalid weather data:', data);
+        }
+      })
+      .catch((err) => console.error('Error fetching weather data:', err));
+  }, []);
+  
+  const fetchWeatherData = async () => {
+    try {
+      const response = await fetch(weatherApiUrl);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  };
+  
   const handleSelect = (val: string) => {
     setSelectedCategories((prev) =>
       prev.includes(val) ? prev.filter((cat) => cat !== val) : [...prev, val]
@@ -30,11 +55,9 @@ const Home = (props: ComponentNavigationProps) => {
       const response = await fetch(URL);
       const data = await response.json();
       if (data.results && Array.isArray(data.results)) {
-        // Ensure that data.results is an array
         setNewsData((prev) => [...prev, ...data.results]);
         setNextPage(data.nextPage);
       } else {
-        // Handle other unexpected cases
         console.error("Invalid data received:", data);
       }
       setIsLoading(false);
@@ -45,10 +68,14 @@ const Home = (props: ComponentNavigationProps) => {
 
   return (
     <View style={styles.container}>
-      <Appbar.Header
-        style={styles.appBarStyle} // Add custom style to the AppBar
-      >
+      <Appbar.Header style={styles.appBarStyle}>
         <Appbar.Content title="Matt News" titleStyle={styles.titleStyle} />
+        {weather && (
+          <View style={styles.weatherContainer}>
+            <Icon name="sun-o" size={30} color="black" style={styles.weatherIcon} />
+            <Text style={styles.temperature}>{weather.main.temp}°C</Text>
+          </View>
+        )}
       </Appbar.Header>
       <View style={styles.filtersContainer}>
         {categories.map((cat) => (
@@ -74,7 +101,7 @@ const Home = (props: ComponentNavigationProps) => {
       </View>
       <ProgressBar visible={isLoading} indeterminate color={MD3Colors.error50} />
       <FlatList
-        keyExtractor={(item) => `${item.title}-${item.publishedAt}`} // Create a unique composite key
+        keyExtractor={(item) => `${item.title}-${item.publishedAt}`}
         onEndReached={() => handlePress()}
         style={styles.flatList}
         data={newsData}
@@ -86,7 +113,7 @@ const Home = (props: ComponentNavigationProps) => {
             image_url={item.image_url}
             title={item.title}
             content={item.content}
-            placeholderImage={placeholderImage} // Pass the placeholder image
+            placeholderImage={placeholderImage}
           />
         )}
       />
@@ -99,10 +126,28 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   appBarStyle: {
-    backgroundColor: 'f9f7f4', // Light gray background color
+    backgroundColor: '#f9f7f4',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   titleStyle: {
-    color: 'black', // Black title text color
+    color: 'black',
+    marginLeft: 60, // Adjust the left margin as needed
+  },
+  weatherContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: 16,
+  },
+  weatherIcon: {
+    width: 30,
+    height: 30,
+  },
+  temperature: {
+    color: 'black',
+    fontSize: 18,
+    marginLeft: 8,
   },
   filtersContainer: {
     flexDirection: "row",
